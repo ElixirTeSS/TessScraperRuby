77dc222a7745292ff66607e431756283b131d93a
Milo Thurston <knirirr@gmail.com>
2015-03-03 16:35:19 +0000

    Converted extract_keywords (untested).

--- ebi_scraper.rb
+++ ebi_scraper.rb
@@ -17,6 +17,22 @@ def parse_data(page)
 end
 
 def extract_keywords(text)
+  # Remove 'Topic:', newlines, and white space.
+  # Split into array of topics and reject any empty ones
+  # Format for CKAN Uploader
+  text.gsub!('Topic:', '')
+  text.gsub!(/\n/, '')
+  text.gsub!(/\s+/,' ')
+  keywords = text.split(' ')
+  tags = []
+  keywords.each do |keyword|
+    if keyword == '' or keyword.length < 3
+        keywords.delete(keyword)
+    else
+        tags << {'name' => keyword.gsub!(/\W+/,'')}
+    end
+  end
+  return tags
 
 end
 
