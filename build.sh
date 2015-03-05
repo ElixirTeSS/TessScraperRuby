f992f7d4a4788a008d159315d2e2eb2d244a152c
Milo Thurston <knirirr@gmail.com>
2015-03-05 16:10:56 +0000

    A script to allow quick rebuilding of the gem whilst testing.

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" 
+rvm use ruby-head@scraper
+cd tess_uploader && gem build tess_uploader.gemspec && gem install tess_uploader-0.0.1.gem 
