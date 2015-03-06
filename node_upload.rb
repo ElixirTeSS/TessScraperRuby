ffa5532bed4fe597bfa031e6bd467848ae2e7a34
Niall Beard <niallbeard@gmail.com>
2015-03-06 16:58:42 +0000

    node names must have no spaces and be downcase. Catch errors too

--- node_upload.rb
+++ node_upload.rb
@@ -36,17 +36,14 @@ end
 
 begin
     nodes = YAML.load_file('nodes.yml')
-rescue Exception
-    print "Problem loading nodes file"
+    nodes.each do |node| 
+        title, info = node
+        name = title.downcase.gsub(' ', '-')
+        node = Node.new(title, name, \
+                        info['country_code'], info['description'], \
+                        info['image_url'], info['tec'], info['trc'], info['hon'])
+        Uploader.create_node(node)
+    end
+rescue 
+    print "Problem loading nodes file \n"
 end
-
-nodes.each do |node| 
-    title, info = node
-    name = title.downcase
-    node = Node.new(title, name, \
-                    info['country_code'], info['description'], \
-                    info['image_url'], info['tec'], info['trc'], info['hon'])
-    Uploader.create_node(node)
-end
-
-
