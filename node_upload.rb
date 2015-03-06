e6b993dc9628cb5bc31b45a50d814368c48c95cd
Niall Beard <niallbeard@gmail.com>
2015-03-06 17:16:41 +0000

    Moved node class to tess_uploader library

--- node_upload.rb
+++ node_upload.rb
@@ -1,39 +1,6 @@
 require 'tess_uploader'
 require 'yaml'  
 
-class Node
-  attr_accessor :title, :name, :country_code, :description, :image_url, :tec, :trc, :hon
-
-  def initialize(title,name,country_code,description=nil,image_url=nil,tec=nil,trc=nil,hon=nil)
-    @type = 'node'
-    @title = title || nil
-    @name = name || nil
-    @country_code = country_code || nil
-    @description = description
-    @trc = trc
-    @tec = tec
-    @hon = hon
-    @image_url = image_url
-  end
-
-  def dump
-    hash = {}
-    self.instance_variables.each do |var|
-      varname = var.to_s.gsub(/@/,'')
-      hash[varname] = self.instance_variable_get var
-    end
-    return hash
-  end
-
-  def to_json
-    return self.dump.to_json
-  end
-
-  def [](var)
-    return self.send(var)
-  end
-end
-
 begin
     nodes = YAML.load_file('nodes.yml')
     nodes.each do |node| 
