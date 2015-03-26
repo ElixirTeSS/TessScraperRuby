70e1f35d95ad4eea64a72da38712e745cd5823d4
Niall Beard <niallbeard@gmail.com>
2015-03-26 11:09:32 +0000

    Added package upload and sib scraper to run script

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 ruby node_upload.rb
+ruby package_upload.rb
 ruby ebi_scraper.rb
 ruby goblet_scraper.rb
 ruby genome3d_scraper.rb
 ruby legacy_software_carpentry_scraper.rb
 ruby coursera_scraper.rb
+ruby sib_scraper.rb
 
